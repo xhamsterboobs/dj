5fa4f2214d96618667bd441973847c797541cc5a
Prasad Raghunath Ghadwaje <prasad.oficial93@gmail.com>
2025-11-10 22:17:53 +0530

    Practical N0 8

--- companySallary_BubbleSort.py
+++ companySallary_BubbleSort.py
@@ -6,6 +6,14 @@ def bubbleSort(salaries):
             if salaries[j]>salaries[j+1]:
                 salaries[j],salaries[j+1]=salaries[j+1],salaries[j]
 
+def topFive(salaries):
+    print("Top five salary :")
+    counter=1
+    for i in range(len(salaries)-1,0,-1):
+        if i >=len(salaries)-5:
+            print(f"Salary rank {counter}:{salaries[i]}")
+    counter+=1
+
 total_employee=int(input("Enter the total number of employee :"))
 print("Enter the salaries of the Employee :")
 salaries=[]
@@ -13,7 +21,10 @@ for i in range(total_employee):
     sl=float(input(f"Enter salary of Employee {i+1} : "))
     salaries.append(sl)
 
-print(f"Salaries of the Employee before Sorting :{salaries}")
+print(f"Salaries of the Employee before Sorting :/n{salaries}")
+
 bubbleSort(salaries)
-print(f"Salaries after the Bubble Sort (Acssending) :{salaries}")
+print(f"Salaries after the Bubble Sort (Acssending) :/n{salaries}")
+
+topFive(salaries)
 print("\n\t-----Thank you ------")
